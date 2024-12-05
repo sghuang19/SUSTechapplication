a0b8bf4038517ff7220143da0b405f5e122fd49a
Samuel Huang <hi@sghuang.com>
2024-12-05 15:08:32 -0500

    Uncomment pubDate processing

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,15 +5,15 @@ const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
-	// FIXME: suppress frontmatter checking for now
-	// schema: z.object({
-	// 	title: z.string(),
+	// FIXME: suppress some frontmatter checking for now
+	schema: z.object({
+		title: z.string(),
 	// 	description: z.string(),
 	// 	// Transform string to Date object
-	// 	pubDate: z.coerce.date(),
+		pubDate: z.coerce.date(),
 	// 	updatedDate: z.coerce.date().optional(),
 	// 	heroImage: z.string().optional(),
-	// }),
+	}),
 });
 
 export const collections = { blog };
