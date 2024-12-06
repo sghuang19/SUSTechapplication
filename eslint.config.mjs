858eb070da87fcb47b582affda17a4d10200ff80
Samuel Huang <hi@sghuang.com>
2024-12-06 14:20:34 -0500

    Integrate Vue to Prettier and ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,17 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import eslintPluginAstro from 'eslint-plugin-astro';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintPluginAstro from "eslint-plugin-astro";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import pluginVue from "eslint-plugin-vue";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,astro}"]},
-  {languageOptions: {globals: globals.browser}},
+  { files: ["**/*.{js,mjs,cjs,ts,astro,vue}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
-  ...eslintPluginAstro.configs.recommended
+  ...eslintPluginAstro.configs.recommended,
+  ...pluginVue.configs["flat/recommended"],
 ];
