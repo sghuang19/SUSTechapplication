c903f820092e97bfe0d33cb8489d8c9b4952dc72
Samuel Huang <hi@sghuang.com>
2024-12-07 16:30:10 -0500

    Create Filter.vue and useFilters.js

---
+++ src/utils/useFilters.js
@@ -0,0 +1,48 @@
+import { reactive, computed, watch } from "vue";
+
+/** core logic for filtering posts inside Vue component */
+export default function useFilters(posts, emit) {
+  // Reactive filters object
+  // TODO: use abstraction from METADATA
+  const filters = reactive({
+    category: "",
+    degree: "",
+    region: "",
+  });
+
+  // Computed filtered posts
+  const filteredPosts = computed(() => {
+    console.log("applying filters");
+    let results = posts;
+    // TODO: abstract filtering, add conditional filter
+    // Apply category filter
+    if (filters.category) {
+      results = results.filter(
+        (post) => post.data.category === filters.category,
+      );
+    }
+
+    // Apply degree filter
+    if (filters.degree) {
+      results = results.filter((post) => post.data.degree === filters.degree);
+    }
+
+    // Apply region filter
+    if (filters.region) {
+      results = results.filter((post) => post.data.region === filters.region);
+    }
+    return results;
+  });
+
+  // emit updated filteredPosts
+  watch(filteredPosts, () => {
+    console.log("Filters changed");
+    console.log(filters);
+    emit("filter-results", filteredPosts.value);
+  });
+
+  return {
+    filters,
+    filteredPosts,
+  };
+}
