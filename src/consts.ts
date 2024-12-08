5600d31b820779b60d691d25781dfece0aedc1e2
Samuel Huang <hi@sghuang.com>
2024-12-08 17:41:22 -0500

    Extract all posts fetching out

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 import useMetadata from "./utils/useMetadata";
+import allPosts from "./utils/allPosts";
 
 export const SITE_TITLE = "Astro Blog";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 export const METADATA = useMetadata();
+export const ALL_POSTS = allPosts;
