e19ee635b8e4fa1535acf1ffe3769b829dc8d7e9
Samuel Huang <hi@sghuang.com>
2024-12-08 12:16:57 -0500

    Introduce DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,16 @@ import daisyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,vue}", "./public/**/*.html"],
+  safelist: [
+    {
+      pattern: /^badge-/, // Dynamic class names not supported in dev mode
+    },
+  ],
   theme: {
     extend: {},
   },
   plugins: ["prettier-plugin-tailwindcss", daisyui],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
 };
