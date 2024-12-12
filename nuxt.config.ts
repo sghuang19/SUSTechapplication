735bb8b5a9d9f802ec63e7e89727d07b8786efb1
Samuel Huang <hi@sghuang.com>
2024-12-12 16:50:22 -0500

    Init Naive UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,31 @@
 import { defineNuxtConfig } from "nuxt/config";
+// Auto import for Nuxt components
+import AutoImport from "unplugin-auto-import/vite";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "nuxtjs-naive-ui"],
+  vite: {
+    plugins: [
+      AutoImport({
+        imports: [
+          {
+            "naive-ui": [
+              "useDialog",
+              "useMessage",
+              "useNotification",
+              "useLoadingBar",
+            ],
+          },
+        ],
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()],
+      }),
+    ],
+  },
 });
