e586b21eb4a429cbd6e57c911fbf705477745fcf
Samuel Huang <hi@sghuang.com>
2024-12-12 16:33:53 -0500

    Migrate dir for Nuxt

--- utils/useMetadata.js
+++ utils/useMetadata.js
@@ -3,7 +3,7 @@ import { load } from "js-yaml";
 import { readFileSync } from "fs";
 
 export default function useMetadata() {
-  const filePath = "./src/metadata.yaml"; // relative to root
+  const filePath = "metadata.yaml"; // relative to root
   const file = readFileSync(filePath);
   return load(file);
 }
