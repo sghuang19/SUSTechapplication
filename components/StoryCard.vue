91081ea3bc3b2b3742215117c08b8cc7a64a1db6
Samuel Huang <hi@sghuang.com>
2024-12-12 22:48:06 -0500

    Create StoryCard.vue and StoryList.vue

---
+++ components/StoryCard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import ChipGroup from "./ChipGroup.vue";
+  defineProps(["story"]);
+</script>
+
+<template>
+  <n-card hoverable :title="story.title" size="medium">
+    <template #header-extra> by [AUTHOR]</template>
+    <template #footer>
+      <ChipGroup :story="story" />
+    </template>
+  </n-card>
+</template>
---
+++ components/StoryList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import StoryCard from "./StoryCard.vue";
+
+  defineProps(["stories"]);
+</script>
+
+<template>
+  <ul class="">
+    <li v-for="story in stories" :key="story._id">
+      <StoryCard :story="story" />
+    </li>
+  </ul>
+</template>
