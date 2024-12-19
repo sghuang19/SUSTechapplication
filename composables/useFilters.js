4bcffa0de5b5c67b5def247a76a0cd26161c4751
Samuel Huang <hi@sghuang.com>
2024-12-19 00:45:53 -0500

    Fix useFilters

--- composables/useFilters.js
+++ composables/useFilters.js
@@ -2,17 +2,17 @@ export const useFilters = () => {
   // track the active tab
   const tab = useState("tab", () => "grad");
   // tracks choices across different tabs
-  const filters = reactive({
+  const filters = useState("filters", () => ({
     grad: {},
     employment: {},
     recruit: {},
     abroad: {},
     exp: {},
-  });
+  }));
 
   const activeFilter = computed(() => ({
-    category: tab.value, // tab itself is part of the filter
-    ...filters[tab.value],
+    category: [tab.value], // tab itself is part of the filter, everything is in array
+    ...filters.value[tab.value], // spread the rest filters
   }));
 
   return {
