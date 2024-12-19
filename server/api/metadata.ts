d9bf02be6a57c84a93950caaf111bfff26d56b6e
Samuel Huang <hi@sghuang.com>
2024-12-18 20:12:01 -0500

    Move metadata.yaml

--- server/api/metadata.ts
+++ server/api/metadata.ts
@@ -4,7 +4,7 @@ import { readFileSync } from "fs";
 import { parse } from "yaml";
 
 export default defineEventHandler(async (event) => {
-  const filePath = resolve(process.cwd(), "config/metadata.yaml"); // relative to root
+  const filePath = resolve(process.cwd(), "metadata.yaml"); // relative to root
   const file = readFileSync(filePath, "utf8");
   return parse(file);
 });
